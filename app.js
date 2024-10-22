cca8e815731274bc4db3642b57c302b63b4e5bea
yasminleal0 <148580652+yasminleal0@users.noreply.github.com>
2024-10-22 16:03:51 -0300

    jjj

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('contaiiner')
+    let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
@@ -8,6 +8,22 @@ function criaCartao(categoria, pergunta, resposta) {
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
-    </div> 
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
     
     }
---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma funçaõ?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem é considerado o pai da genética?',
+    'Gregor Mendel'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de Pi?',
+    '3,14'
+)
