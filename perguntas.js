023442070991e710238c4e3dafa987f0ea42240b
yasminleal0 <148580652+yasminleal0@users.noreply.github.com>
2024-11-05 11:42:52 -0300

    i

--- perguntas.js
+++ perguntas.js
@@ -12,18 +12,6 @@ criaCartao(
 
 criaCartao(
     'Genetica',
-    'A acondroplasia é um tipo de nanismo em que a cabeça e o tronco são normais, mas braços e pernas são muito curtos. É condicionado por um gene dominante que, em homozigose, provoca a morte antes do nascimento. Os indivíduos normais são recessivos e os afetados são heterozigotos. A probabilidade de um casal acondroplásico ter uma criança de sexo feminino normal é de:',
-    '1/6'
-)
-
-criaCartao(
-    'Genetica',
-    'Uma planta A e outra B, com ervilhas amarelas e de genótipos desconhecidos, foram cruzadas com plantas C que produzem ervilhas verdes. O cruzamento A x C originou 100% de plantas com ervilhas amarelas e o cruzamento B x C originou 50% de plantas com ervilhas amarelas e 50% verdes. Os genótipos das plantas A, B e C são, respectivamente:',
-    'VV, Vv, vv'
-)
-
-criaCartao(
-    'Genetica',
     'Gregor Mendel, considerado o pai ou fundador da genética clássica, realizou experimentos com plantas produtoras de ervilhas. Para demonstrar suas hipóteses, Mendel usou este tipo de vegetal por que:',
     'o androceu e o gineceu estão presentes numa mesma flor, o que facilita a ocorrência da autofecundação.'
 )
